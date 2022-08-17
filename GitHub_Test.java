8ad8dddefa55ace9fc646a5cb68304ecfea1fb2b
Dylan Ellis <dylan00ellis@gmail.com>
2022-08-17 09:39:02 +0200

    added output

--- GitHub_Test.java
+++ GitHub_Test.java
@@ -4,6 +4,7 @@ public class GitHub_Test {
     
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Do not fail the theory test");
         // This is my comment
     }
 }
